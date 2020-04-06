2c4e2b05b407b5d684707813d83121fe049ffb99
harmonious-founder-9456 <marvelljgarrett@gmail.com>
2020-04-06 02:57:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,8 @@
-def introduction_with_language(name, language)
+def introduction('Josh')
+  puts "Hi, my name is #{Josh}."
+end
+
+def introduction_with_language('name', 'language')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
